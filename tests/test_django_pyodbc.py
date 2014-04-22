12dda0b7f7d87d6e6e8f42fb88f76210cfffecf3
Jordano Celestrini <jordanorc@Jordanos-MacBook-Air.local>
2014-04-22 18:40:03 -0300

    Update project to be used with Pytds

--- tests/test_django_pyodbc.py
+++ tests/test_django_pyodbc.py
@@ -11,35 +11,30 @@
 # situations, so it is recommended to run the test suite against as many
 # database backends as possible.  You may want to create a separate settings
 # file for each of the backends you test against.
-
-
 DATABASES = {
-   'default': {
+    'default': {
        'ENGINE': "django_pyodbc",
-       'HOST': "127.0.0.1\SQLEXPRESS,1433",
+       'HOST': "10.37.129.3",
        'USER': "sa",
-       'PASSWORD': "1Password",
+       'PASSWORD': "admin",
        'NAME': "defaultdb",
        'OPTIONS': {
            'host_is_server': True,
            'autocommit': True,
-           'driver': "SQL Server Native Client 11.0"
        },
-    }, 
+    },
    'other': {
        'ENGINE': "django_pyodbc",
-       'HOST': "127.0.0.1\SQLEXPRESS,1433",
+       'HOST': "10.37.129.3",
        'USER': "sa",
-       'PASSWORD': "1Password",
+       'PASSWORD': "admin",
        'NAME': "otherdb",
        'OPTIONS': {
            'host_is_server': True,
            'autocommit': True,
-           'driver': "SQL Server Native Client 11.0"
        },
     }, 
 }
-
 SECRET_KEY = "django_tests_secret_key"
 
 # Use a fast hasher to speed up tests.
