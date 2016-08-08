69f7ec4153ae3363338e5e4d303eb10132eb90f6
Dan Loewenherz <dan@lionheartsw.com>
2016-08-08 16:04:59 -0500

    bump version to 0.4.2

--- django_pyodbc/metadata.py
+++ django_pyodbc/metadata.py
@@ -1,17 +1,3 @@
-# Copyright 2013 Lionheart Software LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # Copyright (c) 2008, django-pyodbc developers (see README.rst).
 # All rights reserved.
 #
@@ -40,8 +26,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __maintainer__ = "Dan Loewenherz"
 __maintainer_email__ = "dan@lionheartsw.com"
-__license__ = "License :: OSI Approved :: BSD License"
+__license__ = "BSD 3-Clause License"
 
